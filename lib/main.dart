c06d17b04733eb98faf4654a4b5f51eedabe685d
iam-talhaa <tahakhan4141@gmail.com>
2025-02-04 22:50:43 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app_ui/splashscreen/splash_screen.dart';
-import 'package:travel_app_ui/ui/home/purchase_screen.dart';
 
 void main() {
   runApp(Travel_app());
